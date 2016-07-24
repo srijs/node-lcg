8a2690c09c1f95572b123e89d928025a3423d72e
Sam Rijs <srijs@airpost.net>
2016-07-24 11:54:15 +1000

    perturb seed on construction

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ const A = 1103515245;
 const C = 12345;
 
 export class Random {
-  private _seed: number;
+  private _value: number;
 
   constructor(seed: number) {
-    this._seed = seed & 0x7fffffff;
+    this._value = (A * (seed & 0x7fffffff) + C) % M;
   }
 
   next(): Random {
-    return new Random((A * this._seed + C) % M);
+    return new Random(this._value);
   }
 
   get(): number {
-    return this._seed / M;
+    return this._value / M;
   }
 }
